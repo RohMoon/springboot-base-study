788f6f9022e4b4f1378993f9ac56db6ecd2846a5
RohMoon <rs.moony@gmail.com>
2023-07-29 12:22:22 +0900

    refactor: (OrderController.java) 불필요 import 삭제

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -2,10 +2,7 @@ package com.study.base.boot.aggregations.v1.order.presentation;
 
 import com.study.base.boot.config.annotations.RestApi;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestApi("/v1/orders")
