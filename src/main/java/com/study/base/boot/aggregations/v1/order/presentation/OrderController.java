cbf72e81037e996bf50d16be9c0a77d6484bb159
RohMoon <rs.moony@gmail.com>
2023-07-29 12:08:18 +0900

    feat: OrderController.java 최초 작성

---
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -0,0 +1,10 @@
+package com.study.base.boot.aggregations.v1.order.presentation;
+
+import com.study.base.boot.config.annotations.RestApi;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestApi("/v1/orders")
+public class OrderController {
+
+}
