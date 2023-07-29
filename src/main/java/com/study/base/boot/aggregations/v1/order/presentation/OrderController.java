e7f657da97bbd05fb0676931d10616c45501c08e
RohMoon <rs.moony@gmail.com>
2023-07-29 12:12:16 +0900

    feat: (OrderController.java) getOrders 메서드 예제 추가

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -1,10 +1,18 @@
 package com.study.base.boot.aggregations.v1.order.presentation;
 
 import com.study.base.boot.config.annotations.RestApi;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestApi("/v1/orders")
 public class OrderController {
 
+    @GetMapping
+    public List<String> getOrders() {
+        return List.of("A", "B", "C");
+    }
 }
