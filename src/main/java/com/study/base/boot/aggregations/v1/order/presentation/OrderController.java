3c55a3d77990cc3d675cf2efdd429283acdb60b4
RohMoon <rs.moony@gmail.com>
2023-07-29 12:36:18 +0900

    refactor: (OrderController.java) getOrders Http Method 변경

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -1,14 +1,14 @@
 package com.study.base.boot.aggregations.v1.order.presentation;
 
+import com.study.base.boot.config.annotations.Get;
 import com.study.base.boot.config.annotations.RestApi;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @RestApi("/v1/orders")
 public class OrderController {
 
-    @GetMapping
+    @Get
     public List<String> getOrders() {
         return List.of("A", "B", "C");
     }
