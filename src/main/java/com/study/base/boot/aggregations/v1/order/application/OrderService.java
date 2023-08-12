b72eb08b311574b3e41cd333bf337657c70a8ba8
RohMoon <rs.moony@gmail.com>
2023-08-12 12:48:09 +0900

    refactor: (OrderService.java) : 오더주문에 대해 대응 하여 변경

--- src/main/java/com/study/base/boot/aggregations/v1/order/application/OrderService.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/application/OrderService.java
@@ -1,6 +1,9 @@
 package com.study.base.boot.aggregations.v1.order.application;
 
+import com.study.base.boot.aggregations.v1.order.application.dto.req.CreateOrder;
+import com.study.base.boot.aggregations.v1.order.domain.OrderAggregate;
 import com.study.base.boot.aggregations.v1.order.infrastructure.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,12 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    @Transactional
+    public void create(CreateOrder createOrder) {
+        final var orderAggregate = OrderAggregate.builder()
+                .build()
+                .patch(createOrder)
+                .create(orderRepository);
+    }
 }
