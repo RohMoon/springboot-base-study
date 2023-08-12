e695c5cc101bd773bd35c1143b4e72d502c9f6dd
RohMoon <rs.moony@gmail.com>
2023-08-12 12:46:47 +0900

    refactor: (CreateOrder) : patch 추가 및 create 추가로 체인가능하게끔 변경

--- src/main/java/com/study/base/boot/aggregations/v1/order/domain/OrderAggregate.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/domain/OrderAggregate.java
@@ -1,14 +1,23 @@
 package com.study.base.boot.aggregations.v1.order.domain;
 
+import com.study.base.boot.aggregations.v1.order.application.dto.req.CreateOrder;
+import com.study.base.boot.aggregations.v1.order.infrastructure.repository.OrderRepository;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(catalog = "base", name = "order")
 public class OrderAggregate {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK값을 어떻게 생성할거니 ai
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String orderNumber;
@@ -17,8 +26,25 @@ public class OrderAggregate {
     private int price;
     private int deliveryFee;
     private String address;
-
     private long userId;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
+
+    public OrderAggregate create(OrderRepository orderRepository) {
+        orderRepository.save(this);
+
+        return this;
+    }
+
+    public OrderAggregate patch(CreateOrder createOrder) {
+        this.orderNumber = StringUtils.defaultIfEmpty(createOrder.getOrderNumber(), this.orderNumber);
+        this.orderName = StringUtils.defaultIfEmpty(createOrder.getOrderNumber(), this.orderName);
+        this.price = createOrder.getPrice();
+        this.deliveryFee = createOrder.getDeliveryFee();
+        this.address = StringUtils.defaultIfEmpty(createOrder.getOrderNumber(), this.address);
+        this.userId = createOrder.getUserId();
+        this.createdDate = LocalDateTime.now();
+
+        return this;
+    }
 }
