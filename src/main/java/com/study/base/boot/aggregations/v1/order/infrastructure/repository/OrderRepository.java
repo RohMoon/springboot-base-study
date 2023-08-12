31598998796fc5712c8a9b18165405da317a44c0
RohMoon <rs.moony@gmail.com>
2023-08-12 12:10:47 +0900

    refactor: (OrderAggregate.java) JpaRepository<OrderAggregate, Long> { - JpaRepository : jpa에서 지원하는기본 메서드 - <테이블 매핑 객체 타입, PK 타입> }

--- src/main/java/com/study/base/boot/aggregations/v1/order/infrastructure/repository/OrderRepository.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/infrastructure/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.study.base.boot.aggregations.v1.order.infrastructure.repository;
 
-public interface OrderRepository {
+import com.study.base.boot.aggregations.v1.order.domain.OrderAggregate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderAggregate, Long> {
 }
