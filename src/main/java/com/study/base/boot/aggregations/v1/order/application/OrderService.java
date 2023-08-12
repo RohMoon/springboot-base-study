fc20fca8d1e04ad727803584f21596b85a7c335b
RohMoon <rs.moony@gmail.com>
2023-08-12 12:21:43 +0900

    refactor: (OrderService) : 어노테이션등 추가

--- src/main/java/com/study/base/boot/aggregations/v1/order/application/OrderService.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/application/OrderService.java
@@ -1,4 +1,12 @@
 package com.study.base.boot.aggregations.v1.order.application;
 
+import com.study.base.boot.aggregations.v1.order.infrastructure.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
 }
