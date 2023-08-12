917253d74bb6ef7205848eda87cf6b1935361646
RohMoon <rs.moony@gmail.com>
2023-08-12 12:00:11 +0900

    feat: (OrderAggregate) 추가 컬럼 필드 추가

--- src/main/java/com/study/base/boot/aggregations/v1/order/domain/OrderAggregate.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/domain/OrderAggregate.java
@@ -2,10 +2,23 @@ package com.study.base.boot.aggregations.v1.order.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(catalog = "base", name = "order")
 public class OrderAggregate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK값을 어떻게 생성할거니 ai
     private Long id;
+
+    private String orderNumber;
+    private String orderName;
+    private String status;
+    private int price;
+    private int deliveryFace;
+    private String address;
+
+    private long userId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
