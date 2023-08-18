b03ea31e7d58f05c1ff0f2790389a1de9f57b827
RohMoon <rs.moony@gmail.com>
2023-08-18 17:11:11 +0900

    refactor: (CreateOrdersDto.java) : @Size 적용

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/dto/req/CreateOrdersDto.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/dto/req/CreateOrdersDto.java
@@ -2,6 +2,7 @@ package com.study.base.boot.aggregations.v1.order.presentation.dto.req;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -12,7 +13,8 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateOrdersDto {
 
-    @NotNull
     @Valid
+    @NotNull
+    @Size(min = 1)
     private List<CreateOrderDto> createOrders;
 }
