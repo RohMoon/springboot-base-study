ffd1bfee461b14090c82ed731e652204599ed989
RohMoon <rs.moony@gmail.com>
2023-08-18 14:37:46 +0900

    feat: (CreateOrdersDto.java) : 다건 입력 dto 생성

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/dto/req/CreateOrdersDto.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/dto/req/CreateOrdersDto.java
@@ -1,33 +1,18 @@
 package com.study.base.boot.aggregations.v1.order.presentation.dto.req;
 
-import com.study.base.boot.aggregations.v1.order.application.dto.req.CreateOrder;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
-@Builder
+@Builder(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateOrdersDto {
-    @NotNull
-    private String orderNumber;
 
     @NotNull
-    private String orderName;
-    private int price;
-    private int deliveryFee;
-
-    @NotNull
-    private String address;
-    private long userId;
-
-    public CreateOrder toCreate() {
-        return CreateOrder.builder()
-                .orderNumber(this.orderNumber)
-                .orderName(this.orderName)
-                .price(this.price)
-                .deliveryFee(this.deliveryFee)
-                .address(this.address)
-                .userId(this.userId)
-                .build();
-    }
+    @Valid
+    private List<CreateOrderDto> createOrders;
 }
